455bf2856bc52044bab786763c39f3f8e279c69f
Sergiy Homenuk <sergijgomenuk63@gmail.com>
2026-09-06 00:31:15 +0300

    fix: remove .env from tracking

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config();
+
 app.get('/', (req, res) => {
     res.send('Bot is running!');
 });
